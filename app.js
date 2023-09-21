9d324ca683cb2637d2ceccfcc2c685f4ccdc85e5
Saishree9 <saishreegit@gmail.com>
2023-09-21 19:42:02 +0530

    JS practise1 (Array method)

---
+++ app.js
@@ -0,0 +1,73 @@
+// let products=["mobile","watch","earphones","camera","watch","mobile","laptop"]
+// let s="javascript"
+// console.log(products.join(""));
+
+// ! --------Array.isArray method-----------------
+// console.log(Array.isArray(products));
+
+// console.log(Array.isArray(s));
+
+
+// ! --------Array.from method-----------------
+// let res=Array.from(s)
+// console.log(res);
+
+// console.log(Array.isArray(res));
+
+// ! --------set method-----------------
+
+// let numbers=["3","1","8","32","11","3","7","8","12","9"]
+// console.log(numbers);
+
+// let res1=new Set(numbers)
+// console.log(res1);
+
+// let newArray=Array.from(res1)
+// console.log(newArray.sort((a,b)=>a-b));  //call-back function for ascending order
+
+// ?--------------------------------------------------------------------------------------
+
+// let p="javascript"
+// console.log(p);
+// let newP=p.split("")
+// // console.log(newP);
+// let rev=newP.reverse()
+// // console.log(rev);
+// let newString=rev.join("")
+// console.log(newString);
+
+// ?--------------------------------------------------------------------------------------
+// let numbers=[3,1,8,32,11,3,7,8,12,9]
+
+// let sum=0;
+
+// for (let i in numbers){
+//     sum+=numbers[i]
+// }
+
+// console.log(sum);
+
+// ?--------------------------------------------------------------------------------------
+
+// let n = [1,2,3];
+// let r1 = n.reduce((a,b,c,d)=>{
+//     return a+b;
+// }
+
+// )
+// console.log(r1);
+// ?--------------------------------------------------------------------------------------
+
+// let numbers=[1,2,3,4]
+
+// console.log(numbers.reduce((a,b,c,d)=>a+b));
+// ?--------------------------------------------------------------------------------------
+
+// let products=["mobile","watch","earphones","camera","watch","mobile","laptop"]
+
+// for(let ele of products){
+//     if(ele[0]!=='m')
+//     console.log(ele);
+// }
+
+// ?--------------------------------------------------------------------------------------
